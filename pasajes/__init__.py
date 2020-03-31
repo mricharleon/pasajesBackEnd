9bdedd08b25b5fad6d4036e74d27daf2b0ff1b50
Richar León <mricharleon@gmail.com>
2020-03-30 20:06:48 -0500

    Autenticación y manejo de sesiones con redis (#6)

--- pasajes/__init__.py
+++ pasajes/__init__.py
@@ -1,17 +1,22 @@
 from pyramid.config import Configurator
 from pyramid.events import NewRequest
 
+from pyramid_redis_sessions import session_factory_from_settings
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
 
     with Configurator(settings=settings) as config:
+        session_factory = session_factory_from_settings(settings)
         allow_cors(config)
         config.include('pyramid_jinja2')
         config.include('.models')
         config.include('.routes')
         config.include('.security')
+        config.set_session_factory(session_factory)
+        config.include('pyramid_redis_sessions')
         config.scan()
     return config.make_wsgi_app()
 
@@ -22,7 +27,7 @@ def allow_cors(config):
             #     request.url
             # ))
             response.headers.update({
-                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Origin': 'http://localhost:9000',
                 'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,HEAD,OPTIONS',
                 'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Accept-Language, X-Request-ID,Authorization',
                 'Access-Control-Allow-Credentials': 'true',
--- pasajes/api/usuarios.py
+++ pasajes/api/usuarios.py
@@ -8,3 +8,9 @@ class RepositorioUsario:
         query_usuarios = request.dbsession.query(User).filter_by().all()
 
         return query_usuarios
+
+    @classmethod
+    def get_usuario(cls, request, id_usuario):
+        query_usuario = request.dbsession.query(User).filter(User.id == id_usuario).first()
+
+        return query_usuario
